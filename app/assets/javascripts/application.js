b2cad08076c8ba3089759b8b60fad1761bcd799b
Thomas Barr <thomas.barr1983@gmail.com>
2019-09-24 20:10:32 +0000

    grammable3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
-//= require jquery
-//= require jquery_ujs
