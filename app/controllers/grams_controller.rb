8e446aef10c420b8a0efa57669005bf579ba92a3
Thomas Barr <thomas.barr1983@gmail.com>
2019-10-01 14:30:00 +0000

    grammable9

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,19 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
     @gram = Gram.new
   end
@@ -9,6 +22,13 @@ class GramsController < ApplicationController
   end
 
   def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
+
+  def edit
     @gram = Gram.find_by_id(params[:id])
     if @gram.blank?
       render plain: 'Not Found :(', status: :not_found
@@ -30,4 +50,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
+  end
 end
